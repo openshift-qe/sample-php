4229af76a184d307afe5893670d914c5779a7862
wzheng1 <wzheng@redhat.com>
2015-05-11 15:13:09 +0800

    p

--- index.php
+++ index.php
@@ -3,21 +3,40 @@ require 'vendor/autoload.php';
 $mail_object =& Mail::factory('sendmail', array("sendmail_path" => "/usr/sbin/sendmail"));
 print_r($mail_object);
 echo "<br />";
+$con = mysql_connect("172.17.0.5","user","pass");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
 
-header('content-type:text/html;charset=utf-8');
-try {  
-    $db = new PDO('mysql:host=172.17.0.5;dbname=root', 'user', 'pass');  
-    //查询  
-	$rows = $db->query('SELECT * from members')->fetchAll(PDO::FETCH_ASSOC);
-	$rs = array();
-    foreach($rows as $row) {  
-        $rs[] = $row; 
-    }  
-    $db = null;  
-} catch (PDOException $e) {  
-    print "Error!: " . $e->getMessage() . "<br/>";  
-    die();  
-}
-print_r($rs);
+if (mysql_query("CREATE DATABASE test",$con))
+  {
+  echo "Database created";
+  }
+else
+  {
+  echo "Error creating database: " . mysql_error();
+  }
 
+// Create table in test database
+mysql_select_db("test", $con);
+$sql = "CREATE TABLE Persons 
+(
+FirstName varchar(15),
+LastName varchar(15),
+Age int
+)";
+mysql_query($sql,$con);
+mysql_query("INSERT INTO Persons (FirstName, LastName, Age) 
+VALUES ('Tina', 'Wang', '30')");
+
+$result = mysql_query("SELECT * FROM Persons");
+
+while($row = mysql_fetch_array($result))
+  {
+  echo $row['FirstName'] . " " . $row['LastName'];
+  echo "<br />";
+  }
+
+mysql_close($con);
 ?>
