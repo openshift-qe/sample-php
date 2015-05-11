6cadc561669cd501f4962940808825da312b3775
wzheng1 <wzheng@redhat.com>
2015-05-11 14:55:30 +0800

    p

--- index.php
+++ index.php
@@ -3,40 +3,26 @@ require 'vendor/autoload.php';
 $mail_object =& Mail::factory('sendmail', array("sendmail_path" => "/usr/sbin/sendmail"));
 print_r($mail_object);
 echo "<br />";
-$con = mysql_connect("172.17.0.5","user","pass");
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
+$mysqli = mysqli_init();
+if (!$mysqli) {
+    die('mysqli_init failed');
+}
+ 
+if (!$mysqli->options(MYSQLI_INIT_COMMAND, 'SET AUTOCOMMIT = 0')) {
+    die('Setting MYSQLI_INIT_COMMAND failed');
+}
+ 
+if (!$mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5)) {
+    die('Setting MYSQLI_OPT_CONNECT_TIMEOUT failed');
+}
+ 
+if (!$mysqli->real_connect('172.17.0.5', 'user', 'pass', 'root')) {
+    die('Connect Error (' . mysqli_connect_errno() . ') '
+            . mysqli_connect_error());
+}
+ 
+echo 'Success... ' . $mysqli->host_info . "\n";
+ 
+$mysqli->close();
 
-if (mysql_query("CREATE DATABASE test",$con))
-  {
-  echo "Database created";
-  }
-else
-  {
-  echo "Error creating database: " . mysql_error();
-  }
-
-// Create table in test database
-mysql_select_db("test", $con);
-$sql = "CREATE TABLE Persons 
-(
-FirstName varchar(15),
-LastName varchar(15),
-Age int
-)";
-mysql_query($sql,$con);
-mysql_query("INSERT INTO Persons (FirstName, LastName, Age) 
-VALUES ('Tina', 'Wang', '30')");
-
-$result = mysql_query("SELECT * FROM Persons");
-
-while($row = mysql_fetch_array($result))
-  {
-  echo $row['FirstName'] . " " . $row['LastName'];
-  echo "<br />";
-  }
-
-mysql_close($con);
 ?>
