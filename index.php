854ff8b26923c47337b130a6758c372bc11ce590
wzheng1 <wzheng@redhat.com>
2015-05-12 16:26:43 +0800

    p

--- index.php
+++ index.php
@@ -3,20 +3,15 @@ require 'vendor/autoload.php';
 $mail_object =& Mail::factory('sendmail', array("sendmail_path" => "/usr/sbin/sendmail"));
 print_r($mail_object);
 echo "<br />";
-$con = mysqli_connect("172.17.0.10","user","pass");
-if (!$con)
-  {
-  die('Could not connect: ' . mysqli_connect_error());
-  }
+$link = mysqli_connect("172.17.0.10", "user", "pass", "root");
 
-mysqli_query($con,"CREATE TABLE Language LIKE CountryLanguage");
-mysqli_query($con, "INSERT INTO Language VALUES ('DEU', 'Bavarian', 'F', 11.2)");
-mysqli_query($con, "INSERT INTO Language VALUES ('DEU', 'Swabian', 'F', 9.4)");
-#mysqli_commit($con);
-
-if ($result = mysqli_query($con, "SELECT * FROM Language")) {
-    echo($result);
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
 }
-mysqli_close($con);
+else {
+   printf("Database connection succeeds");
+}
 
 ?>
