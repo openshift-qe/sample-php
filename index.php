769eba92b1359b1ca4678075ac106b745d4dc7c4
wzheng1 <wzheng@redhat.com>
2015-05-12 13:08:36 +0800

    p

--- index.php
+++ index.php
@@ -3,38 +3,41 @@ require 'vendor/autoload.php';
 $mail_object =& Mail::factory('sendmail', array("sendmail_path" => "/usr/sbin/sendmail"));
 print_r($mail_object);
 echo "<br />";
-$mysqli = new mysqli("172.17.0.10", "user", "pass");
+
+$link = mysqli_connect("172.17.0.10", "user", "pass","root");
 
 /* check connection */
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
 
 /* Create table doesn't return a resultset */
-if ($mysqli->query("CREATE TEMPORARY TABLE myCity LIKE City") === TRUE) {
+if (mysqli_query($link, "CREATE TEMPORARY TABLE myCity LIKE City") === TRUE) {
     printf("Table myCity successfully created.\n");
 }
 
 /* Select queries return a resultset */
-if ($result = $mysqli->query("SELECT Name FROM City LIMIT 10")) {
-    printf("Select returned %d rows.\n", $result->num_rows);
+if ($result = mysqli_query($link, "SELECT Name FROM City LIMIT 10")) {
+    printf("Select returned %d rows.\n", mysqli_num_rows($result));
 
     /* free result set */
-    $result->close();
+    mysqli_free_result($result);
 }
 
 /* If we have to retrieve large amount of data we use MYSQLI_USE_RESULT */
-if ($result = $mysqli->query("SELECT * FROM City", MYSQLI_USE_RESULT)) {
+if ($result = mysqli_query($link, "SELECT * FROM City", MYSQLI_USE_RESULT)) {
 
     /* Note, that we can't execute any functions which interact with the
        server until result set was closed. All calls will return an
        'out of sync' error */
-    if (!$mysqli->query("SET @a:='this will not work'")) {
-        printf("Error: %s\n", $mysqli->error);
+    if (!mysqli_query($link, "SET @a:='this will not work'")) {
+        printf("Error: %s\n", mysqli_error($link));
     }
-    $result->close();
+    mysqli_free_result($result);
 }
 
-$mysqli->close();
+mysqli_close($link);
+
+
 ?>
