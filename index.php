1882d5bde8d46cc62b432ac36b832ff1065848e9
wzheng1 <wzheng@redhat.com>
2015-05-12 11:18:13 +0800

    p

--- index.php
+++ index.php
@@ -8,35 +8,8 @@ if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }
-
-if (mysql_query("CREATE DATABASE test",$con))
-  {
-  echo "Database created";
-  }
 else
   {
-  echo "Error creating database: " . mysql_error();
+   echo "Database conneciton test successfully";  
   }
-
-// Create table in test database
-mysql_select_db("test", $con);
-$sql = "CREATE TABLE Persons 
-(
-FirstName varchar(15),
-LastName varchar(15),
-Age int
-)";
-mysql_query($sql,$con);
-mysql_query("INSERT INTO Persons (FirstName, LastName, Age) 
-VALUES ('Tina', 'Wang', '30')");
-
-$result = mysql_query("SELECT * FROM Persons");
-
-while($row = mysql_fetch_array($result))
-  {
-  echo $row['FirstName'] . " " . $row['LastName'];
-  echo "<br />";
-  }
-
-mysql_close($con);
 ?>
