b27eb1fa4b28eb46af8a9510654cd002898ab1e6
wzheng1 <wzheng@redhat.com>
2015-05-12 10:52:39 +0800

    p

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require 'vendor/autoload.php';
 $mail_object =& Mail::factory('sendmail', array("sendmail_path" => "/usr/sbin/sendmail"));
 print_r($mail_object);
 echo "<br />";
-$con = mysqli_connect("172.17.0.5","user","pass");
+$con = mysqli_connect("172.17.0.10","user","pass");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
