1c20f5cfa7deb5c140624e311b1858d5dc965148
wzheng1 <wzheng@redhat.com>
2015-06-12 17:20:55 +0800

    p

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
 require 'vendor/autoload.php';
-$mysql_ip = getenv('DATABASE_SERVICE_HOST');
-$mysql_user = getenv('MYSQL_USER');
-$mysql_password = getenv('MYSQL_PASSWORD');
-$mysql_databse = getenv('MYSQL_DATABASE');
 
 @$mail_object =& Mail::factory('sendmail', array("sendmail_path" => "/usr/sbin/sendmail"));
 print_r($mail_object);
 
-$link = mysqli_connect($mysql_ip, $mysql_user, $mysql_password, $mysql_databse);
+$link = mysqli_connect(getenv('DATABASE_SERVICE_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), getenv('MYSQL_DATABASE'));
 
 /* check connection */
 if (mysqli_connect_errno()) {
