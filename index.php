e6b2739e801c44046eec28dac709064b44292acd
wzheng1 <wzheng@redhat.com>
2015-08-05 15:28:36 +0800

    parametize db info

--- index.php
+++ index.php
@@ -4,7 +4,12 @@ require 'vendor/autoload.php';
 @$mail_object =& Mail::factory('sendmail', array("sendmail_path" => "/usr/sbin/sendmail"));
 print_r($mail_object);
 
-$link = mysqli_connect('172.17.0.6','userTDA','RLeOLTVP','root');
+$host = getenv("DATABASE_SERVICE_HOST");
+$database = getenv("MYSQL_DATABASE");
+$username = getenv("MYSQL_USER");
+$password = getenv("MYSQL_PASSWORD");
+
+$link = mysqli_connect('$host','$username','$password','$database');
 
 /* check connection */
 if (mysqli_connect_errno()) {
